5f4c7293afbe699b42c3367483df9c0576513639
andrey <andrii.gorkovenko@immundiagnostik.com>
2019-02-08 17:00:32 +0100

    add style for example commponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,24 +2,30 @@ import {Component, OnInit} from "@angular/core";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <form [formGroup]="testForm">
-        <afe-editor formControlName="editorField"></afe-editor>
-    </form>
-  `,
-  styleUrls: ['./app.component.scss']
+	selector: "app-root",
+	template: `
+      <div class="page">
+          <div class="page__inner">
+              <div class="editor">
+                  <form [formGroup]="testForm">
+                      <afe-editor formControlName="editorField"></afe-editor>
+                  </form>
+              </div>
+          </div>
+      </div>
+	`,
+	styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
-  
-  testForm: FormGroup;
-  
-  constructor(private fb: FormBuilder) {
-  }
-  
-  ngOnInit(): void {
-    this.testForm = this.fb.group({
-      editorField: new FormControl("", Validators.required)
-    });
-  }
+	
+	testForm: FormGroup;
+	
+	constructor(private fb: FormBuilder) {
+	}
+	
+	ngOnInit(): void {
+		this.testForm = this.fb.group({
+			editorField: new FormControl("", Validators.required)
+		});
+	}
 }
